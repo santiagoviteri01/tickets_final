0c21f0ed28be967345467af6f63c2ab2c55c84bf
santiagoviteri01 <santiagoviteripuyol@gmail.com>
2025-05-03 15:29:01 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
 matplotlib.use("Agg")  # Esto debe ir antes de importar pyplot
 import matplotlib.pyplot as plt
 import seaborn as sns
