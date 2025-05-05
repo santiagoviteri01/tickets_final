bbb6aa837077bc51d1ed4e5871159fb8f013f3a2
santiagoviteri01 <santiagoviteripuyol@gmail.com>
2025-05-05 10:15:26 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
 
 # Configuración inicial
 def mostrar_dashboard_analisis(pagados, pendientes, asegurados):
